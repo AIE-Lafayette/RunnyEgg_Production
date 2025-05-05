23f19b9e23ab94b8be56f49f685e86e8db2554c7
charlie-flynn <charlie.flynn.business@gmail.com>
2025-05-05 15:10:09 -0500

    tried to make this player input component work (i have no idea what i'm doing)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,24 +7,13 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float _laneWidth;
-
-    private PlayerInput _playerInput;
-
-    private void Awake()
-    {
-        if (TryGetComponent(out PlayerInput playerInput))
-            _playerInput = playerInput;
-        else
-            Debug.Log("PlayerController: No PlayerInput component!");
-    }
-
     private void Update()
     {
 
     }
 
-    public void MoveLeft()
+    public void Move(InputAction.CallbackContext context)
     {
-
+        InputControl input = context.control;
     }
 }
