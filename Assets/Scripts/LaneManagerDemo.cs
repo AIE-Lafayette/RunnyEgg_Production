16d0d9ea0784d6737ccb461190a91012fb19ab55
charlie-flynn <charlie.flynn.business@gmail.com>
2025-05-09 11:16:56 -0500

    added LaneManagerDemo to try to make something that kinda works (it doesn't work for no apparent reason)

---
+++ Assets/Scripts/LaneManagerDemo.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneManagerDemo : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _player;
+
+    [SerializeField]
+    private Vector3[] _lanePositions;
+
+    [SerializeField]
+    private int _playerLaneIndex;
+
+    public void Start()
+    {
+        _player.transform.position = _lanePositions[_playerLaneIndex];
+    }
+
+    /// <summary>
+    /// Gets the position of the lane in the given index.
+    /// </summary>
+    /// <param name="index">The index given.</param>
+    /// <returns>The position of the lane in the given index, as a Vector3.</returns>
+    public Vector3 GetLanePosition(int index)
+    {
+        if (index < _lanePositions.Length && index >= 0)
+            return _lanePositions[index];
+        else
+            return new Vector3();
+    }
+
+    public void MovePlayerLeft()
+    {
+        if (_playerLaneIndex <= 0)
+            return;
+
+        _playerLaneIndex--;
+        _player.transform.position = _lanePositions[_playerLaneIndex];
+        Debug.Log(_playerLaneIndex);
+        Debug.Log(_player.transform.position);
+    }
+
+    public void MovePlayerRight()
+    {
+        if (_playerLaneIndex >= _lanePositions.Length - 1)
+            return;
+
+        _playerLaneIndex++;
+        _player.transform.position = _lanePositions[_playerLaneIndex];
+        Debug.Log(_playerLaneIndex);
+        Debug.Log(_player.transform.position);
+    }
+}
