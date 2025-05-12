86d1ac38ef1846be1529e5f6f3d9bb98338f6c75
charlie-flynn <charlie.flynn.business@gmail.com>
2025-05-12 14:48:22 -0500

    added i-frames to PlayerLivesBehavior

--- Assets/Scripts/PlayerLivesBehavior.cs
+++ Assets/Scripts/PlayerLivesBehavior.cs
@@ -8,6 +8,11 @@ public class PlayerLivesBehavior : MonoBehaviour
     [SerializeField]
     private int _lives;
 
+    [SerializeField]
+    private float _invincibilityFramesDuration;
+
+    private float _invincibilityFramesTimer;
+
     public UnityEvent OnLifeLost;
 
     public UnityEvent OnAllLivesLost;
@@ -15,17 +20,29 @@ public class PlayerLivesBehavior : MonoBehaviour
 
     public int Lives { get => _lives; }
 
+    private void Update()
+    {
+        if (_invincibilityFramesTimer > 0)
+            _invincibilityFramesTimer -= Time.deltaTime;
+    }
+
     public void LoseLife()
     {
+        // Guard clause
+        if (_invincibilityFramesTimer > 0)
+            return;
+
+        // Decrement lives, then invoke OnAllLivesLost if lives are less than or equal to 0, or OnLifeLost if not. 
         _lives--;
         
-        if (_lives == 0)
+        if (_lives <= 0)
         {
             OnAllLivesLost.Invoke();
         }
-        else if (_lives > 0)
+        else
         {
             OnLifeLost.Invoke();
+            _invincibilityFramesTimer = _invincibilityFramesDuration;
         }
     }
 }
