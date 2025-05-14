b3f93d0e4da5bbbfa49b7b93644163d317e9299a
KevinHall2 <khjiitech@gmail.com>
2025-05-14 12:57:46 -0500

    added additional test prefabs to the scene and made progress on the spawning function of the obstacle manager

--- Assets/Scripts/Managers/LaneManager.cs
+++ Assets/Scripts/Managers/LaneManager.cs
@@ -6,33 +6,37 @@ using UnityEngine;
 
 public class LaneManager : MonoBehaviour
 {
-    public static LaneManager instance;
+    //Instance creation for 
+    //public static LaneManager instance;
 
     Vector3[] _gameLanes;
+    Vector3[] _laneSpawnPositions;
 
-    int _laneAmount = 6;
+    protected int _laneAmount = 3;
 
-    Vector3 _startPositionModifier = new Vector3(0, 0, 15);
+    protected static Vector3 _startPositionModifier = new Vector3(0, 0, 15);
+
+    protected static Vector3 _leftLane = new Vector3(-2,0.5f,0);
+    protected static Vector3 _leftLaneStartPosition = _leftLane + _startPositionModifier;
+
+    protected static Vector3 _middleLane = new Vector3(0,0.5f,0);
+    protected static Vector3 _middleLaneStartPosition = _middleLane + _startPositionModifier;
+
+    protected static Vector3 _rightLane = new Vector3(2,0.5f,0);
+    protected static Vector3 _rightLaneStartPosition = _rightLane + _startPositionModifier;
 
     public void SetupGameLanes()
     {
-        Vector3 _leftLane = new Vector3(-2,0.5f,0);
-        Vector3 _leftLaneStartPosition = _leftLane + _startPositionModifier;
-
-        Vector3 _middleLane = new Vector3(0,0.5f,0);
-        Vector3 _middleLaneStartPosition = _middleLane + _startPositionModifier;
-
-        Vector3 _rightLane = new Vector3(2,0.5f,0);
-        Vector3 _rightLaneStartPosition = _rightLane + _startPositionModifier;
-
-
         _gameLanes = new Vector3[_laneAmount];
         _gameLanes[0] = _leftLane;
-        _gameLanes[1] = _leftLaneStartPosition;
-        _gameLanes[2] = _middleLane;
-        _gameLanes[3] = _middleLaneStartPosition;
-        _gameLanes[4] = _rightLane;
-        _gameLanes[5] = _rightLaneStartPosition;
+        _gameLanes[1] = _middleLane;
+        _gameLanes[2] = _rightLane;
+
+
+        _laneSpawnPositions = new Vector3[_laneAmount];
+        _laneSpawnPositions[0] = _leftLaneStartPosition;
+        _laneSpawnPositions[1] = _middleLaneStartPosition;
+        _laneSpawnPositions[2] = _rightLaneStartPosition;
     }
 
     int GetPlayerLane()
@@ -40,42 +44,45 @@ public class LaneManager : MonoBehaviour
         return 0;
     }
 
-    int GetObstacleLane()
+    Vector3 GetObstacleLane()
     {
         if (transform.position.x == _gameLanes[0].x)
         {
-            return 1;
+            return _gameLanes[0];
         }
         else if (transform.position.x == _gameLanes[1].x)
         {
-            return 2;
+            return _gameLanes[1];
         }
         else if (transform.position.x == _gameLanes[2].x)
         {
-            return 3;
+            return _gameLanes[2];
         }
-        else { return 0; }
+        else { return new Vector3(); }
     }
 
+    //Sets an obstacle's lane to one of the three positions.
+    //The parameter int laneNumber determines which lane the obstacle gets set to.
+    //1 is the leftmost lane, 2 is the middle lane, 3 is the rightmost lane.
     public void SetObstacleLane(int laneNumber)
     {
         switch (laneNumber)
         {
             case 1:
-                transform.position = _gameLanes[1];
+                transform.position = _laneSpawnPositions[0];
                 break;
 
             case 2:
-                transform.position = _gameLanes[3];
+                transform.position = _laneSpawnPositions[1];
                 break;
 
             case 3:
-                transform.position = _gameLanes[5];
+                transform.position = _laneSpawnPositions[2];
                 break;
 
             //sets the middle lane to be the default
             default:
-                transform.position = _gameLanes[3];
+                transform.position = _laneSpawnPositions[1];
                 return;
 
         }
--- Assets/Scripts/Managers/MultiLaneObstacleManager.cs
+++ Assets/Scripts/Managers/MultiLaneObstacleManager.cs
@@ -4,20 +4,49 @@ using UnityEngine;
 
 public class MultiLaneObstacleManager : MonoBehaviour
 {
+    //Instance of the LaneManager class to make use of the lane positions
     public LaneManager _laneManager;
+
+    public GameObject[] _obstaclePrefabs;
 
     [SerializeField]
     private float _obstacleSpeed = 5.0f;
 
-    void Start()
+    [SerializeField]
+    private float _obstacleStartDelay = 5.0f;
+
+    [SerializeField]
+    private float _obstacleSpawnInterval = 4.0f;
+
+    private bool _isGameStarted = false;
+
+    void SpawnObstacles()
     {
         _laneManager.SetupGameLanes();
-        _laneManager.SetObstacleLane(2);
+        int obstacleIndex = Random.Range(0, _obstaclePrefabs.Length);
+
+        //int spawnPositionIndex = Random.Range(0, _laneManager._laneAmount + 1);
+        //Vector3 spawnPosition = new Vector3(_laneSpawnPositions[spawnPositionIndex].x, _laneSpawnPositions[spawnPositionIndex].y, _laneSpawnPositions[spawnPositionIndex].z);
+        //Instantiate()
     }
 
-    // Update is called once per frame
-    void Update()
+    void MoveObstacles()
     {
         transform.position = transform.position + (Vector3.back * _obstacleSpeed) * Time.deltaTime;
+    }
+
+    void Start()
+    {
+        if (_isGameStarted == false)
+        {
+            _isGameStarted = true;
+            InvokeRepeating("SpawnObstacles", _obstacleStartDelay, _obstacleSpawnInterval);
+        }
+
+    }
+
+    void Update()
+    {
+        MoveObstacles();
     }
 }
