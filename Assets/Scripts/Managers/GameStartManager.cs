79f0e4b921a403db122ff2367ca0d83bef8726a4
charlie-flynn <charlie.flynn.business@gmail.com>
2025-05-21 11:59:47 -0500

    made GameStartManager and made it able to move the camera to a cooler position

--- Assets/Scripts/Managers/GameStartManager.cs
+++ Assets/Scripts/Managers/GameStartManager.cs
@@ -4,15 +4,82 @@ using UnityEngine;
 
 public class GameStartManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public struct CameraTransform
     {
-        
+        public Vector3 position;
+        public Vector3 rotation;
+        public CameraTransform(Vector3 position, Vector3 rotation)
+        {
+            this.position = position;
+            this.rotation = rotation;
+        }
+    }
+    
+    [SerializeField]
+    private Camera _gameCamera;
+
+    [SerializeField]
+    private CameraTransform _titleCameraTransform;
+
+    [SerializeField]
+    private CameraTransform _gameplayCameraTransform;
+
+    [SerializeField]
+    [Tooltip("The time it will take to move the camera at the start of the game <i>in frames.</i>")]
+    private uint _cameraMoveTime;
+
+    private void Start()
+    {
+        // set the camera position to the user set target position & rotation
+        _gameCamera.transform.position = _titleCameraTransform.position;
+        _gameCamera.transform.rotation = Quaternion.Euler(_titleCameraTransform.rotation);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F))
+            StartGame();
+    }
+
+    public void StartGame()
+    {
+        // preddy quickly but not instantly move the camera to the gameplay camera position & rotation
+        // coroutine would work really !
+        StartCoroutine("MoveCamera");
+    }
+
+    private IEnumerator MoveCamera()
+    {
+        // do some quick math to figure out how much the camera needs to move & rotate per frame
+        float cameraMovementPerFrameX = (_gameplayCameraTransform.position.x - _titleCameraTransform.position.x) / _cameraMoveTime;
+        float cameraMovementPerFrameY = (_gameplayCameraTransform.position.y - _titleCameraTransform.position.y) / _cameraMoveTime;
+        float cameraMovementPerFrameZ = (_gameplayCameraTransform.position.z - _titleCameraTransform.position.z) / _cameraMoveTime;
+
+        Vector3 cameraMovementPerFrameVector = new Vector3(cameraMovementPerFrameX, cameraMovementPerFrameY, cameraMovementPerFrameZ);
+
+        float cameraRotationPerFrameX = (_gameplayCameraTransform.rotation.x - _titleCameraTransform.rotation.x) / _cameraMoveTime;
+        float cameraRotationPerFrameY = (_gameplayCameraTransform.rotation.y - _titleCameraTransform.rotation.y) / _cameraMoveTime;
+        float cameraRotationPerFrameZ = (_gameplayCameraTransform.rotation.z - _titleCameraTransform.rotation.z) / _cameraMoveTime;
+
+        Vector3 cameraRotationPerFrameVector = new Vector3(cameraRotationPerFrameX, cameraRotationPerFrameY, cameraRotationPerFrameZ);
+
+        // move & rotate the camera
+        for (int i = 0; i < _cameraMoveTime; i++)
+        {
+            Vector3 newCameraPosition = _gameCamera.transform.position;
+            Vector3 newCameraRotation = _gameCamera.transform.rotation.eulerAngles;
+
+            newCameraPosition += cameraMovementPerFrameVector;
+            newCameraRotation += cameraRotationPerFrameVector;
+
+            _gameCamera.transform.position = newCameraPosition;
+            _gameCamera.transform.rotation = Quaternion.Euler(newCameraRotation);
+
+            yield return null;
+        }
+
+        _gameCamera.transform.position = _gameplayCameraTransform.position;
+        _gameCamera.transform.rotation = Quaternion.Euler(_gameplayCameraTransform.rotation);
     }
 }
